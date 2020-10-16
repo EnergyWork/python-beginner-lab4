1b13d54b5cd588e47ce1939d8a8d559d462538d8
Artyom <ag.energy@mail.ru>
2020-10-17 01:24:57 +0300

    upd

--- task6/task6.py
+++ task6/task6.py
@@ -1,4 +1,5 @@
 import os, shutil
+import copy
 
 curr_dir = os.getcwd() + f'\\task6'
 shop1 = '\\shop1.txt'
@@ -16,6 +17,18 @@ with open(curr_dir + shop1, encoding='utf-8') as shop1_file, \
 print(shop1_dict)
 print(shop2_dict)
 
+if len(shop1_dict) < len(shop2_dict):
+    shop1_dict, shop2_dict = shop2_dict, shop1_dict
+
+shop_max_file = open(curr_dir + '\\shop_max.txt', 'w', encoding='utf-8') # TODO : Идем только по товарам шоп1, если товар есть в шоп2, но нет в шоп1, то он не будет добавлен в шоп_мах
 shop_max = {}
-for product in shop1_dict if len(shop1_dict) > len(shop2_dict) else shop2_dict:
-    pass
+for product in shop1_dict:
+    if shop2_dict.get(product) is not None:
+        if shop1_dict[product] >= shop2_dict[product]:
+            shop_max_file.write(f'{product} {shop1_dict[product]}\n')
+        else:
+            shop_max_file.write(f'{product} {shop2_dict[product]}\n')
+    else:
+         shop_max_file.write(f'{product} {shop1_dict[product]}\n')
+
+shop_max_file.close()   
